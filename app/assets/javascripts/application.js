3f45ed63a0abe5fc4ae1a40fd1d7f9b9bd75d826
Alex <alex-ju@users.noreply.github.com>
2017-08-09 16:47:53 +0100

    New task-list design and copywriting from Ellen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,14 +36,29 @@ $(document).ready(function () {
 
   // Task list
   // Check for completed tasks and allow access only to the next one
+  // var $previousTask
+  // $('.task-list-item').each(function () {
+  //   var previousCompleted = false
+  //   if ($previousTask) {
+  //     previousCompleted = $previousTask.find('.task-completed').length
+  //     var thisCompleted = $(this).find('.task-completed').length
+  //     if (!thisCompleted && !previousCompleted) {
+  //       $(this).html($(this).find('.task-name').html())
+  //     }
+  //   }
+  //   $previousTask = $(this)
+  // })
+
   var $previousTask
-  $('.task-list-item').each(function () {
+  $('.task-list li').each(function () {
     var previousCompleted = false
     if ($previousTask) {
       previousCompleted = $previousTask.find('.task-completed').length
       var thisCompleted = $(this).find('.task-completed').length
       if (!thisCompleted && !previousCompleted) {
-        $(this).html($(this).find('.task-name').html())
+        $(this).find('.start-section').hide()
+        $(this).find('.task-unavailable').show()
+      } else {
       }
     }
     $previousTask = $(this)
