bdfcc19e6d3b352bbd51cea24840657fb8744af8
Alex <alex-ju@users.noreply.github.com>
2017-08-09 18:09:47 +0100

    Moved task-list logic to the back-end

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,40 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/task-list', function (req, res) {
+  var taskListItems = [
+    {index: '1', title: 'Local council', url: 'licensing-council', description: 'Find the council you need to apply to', values: ['licensing-council'], status: ''},
+    {index: '2', title: 'Previous event details', url: 'previous-events', description: 'Provide details of any events you’ve held in the last year.', values: ['previousLicence'], status: ''},
+    {index: '3', title: 'Event details', url: 'event-start-date', description: 'Confirm activites, dates and location for this application.', values: ['event-start-day'], status: ''},
+    {index: '4', title: 'Applicant details', url: 'applicant-details', description: 'Supply contact information.', values: ['applicant-name'], status: ''},
+    {index: '5', title: 'Complete application', url: 'check-your-answers', description: 'Check your application, agree to the terms and conditions, and pay the £21 fee.', values: [], status: ''}
+  ]
+
+  var lastCompleted = 1
+  for (var i = 0; i < taskListItems.length; i++) {
+    if (taskListItems[i].values) {
+      var completed = false
+      var partlyCompleted = false
+      taskListItems[i].values.forEach((value, index) => {
+        if (req.session.data[value]) {
+          partlyCompleted = true
+          completed = true
+        } else {
+          completed = false
+        }
+      })
+      if (completed) {
+        taskListItems[i].status = 'completed'
+      } else if (partlyCompleted) {
+        taskListItems[i].status = 'partlyCompleted'
+      } else if (i === 0 || taskListItems[i-1].status === 'completed') {
+        taskListItems[i].status = 'actionable'
+      }
+    }
+  }
+  res.render('task-list', {items: taskListItems})
+})
+
 router.get('/start-page', function (req, res) {
   req.session.destroy()
   res.render('start-page')
