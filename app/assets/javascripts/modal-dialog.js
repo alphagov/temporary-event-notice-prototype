43ab83b0823e2dbffbcd7e142359ffbe7014411c
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2017-09-13 16:39:23 +0100

    Debugging code for screen readers

--- app/assets/javascripts/modal-dialog.js
+++ app/assets/javascripts/modal-dialog.js
@@ -24,7 +24,7 @@
 
     bindUIElements: function () {
 
-      //setTimeout(GOVUK.modalDialog.openDialog, 2000) //debug
+      setTimeout(GOVUK.modalDialog.openDialog, 2000) //debug
 
       GOVUK.modalDialog.$openButton.on('click', function (e) {
         GOVUK.modalDialog.openDialog()
